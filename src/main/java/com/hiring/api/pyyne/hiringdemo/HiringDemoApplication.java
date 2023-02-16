77e45cefaf3e758863549a3deca988a495a067fc
Raissa C. Correia <raideveloper@proton.me>
2023-02-16 10:09:01 -0300

    Spring Initializer Done

---
+++ src/main/java/com/hiring/api/pyyne/hiringdemo/HiringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hiring.api.pyyne.hiringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiring/api/pyyne/hiringdemo/HiringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiring.api.pyyne.hiringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
